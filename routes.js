c0546249b843a1069ebda2154bb42e6e339a8a11
Talha Zubair Mayo <tkashi328@gmail.com>
2022-10-19 13:13:56 +0500

    0113

--- routes.js
+++ routes.js
@@ -23,7 +23,7 @@ router.post('/createMsg', async (req, res) => {
 })
 router.get('/createMsg', (req, res) => {
     var challenge = req.query['hub.challenge'];
-    res.status(200).json(challenge);
+    res.status(200).send(challenge);
 })
 
 
